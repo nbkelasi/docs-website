4ece16ad6650a9a8104f7e092094a54ff2a68979
zx <zx@zx.com>
2025-05-11 16:27:10 +0800

    fix config sidebar 重复 and md中图片路径问题

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -52,17 +52,28 @@ export default defineConfig({
         },
       },
     },
+    sidebar: (path) => {
+      // 根据路径动态返回侧边栏配置
+      const sidebarConfig = {
+        "/front_end/nodejs": set_sidebar("/front_end/nodejs"),
+        "/backend/JavaScript": set_sidebar("/backend/JavaScript"),
+        "/backend/html5": set_sidebar("/backend/html5"),
+        "/backend/vue2": set_sidebar("/backend/vue2"),
+        "/backend/react": set_sidebar("/backend/react"),
+        "/backend/Interview_about": set_sidebar("/backend/Interview_about"),
+        "/tool_docs/git": set_sidebar("/tool_docs/git"),
+      };
 
-    sidebar: {
-      "/front_end/nodejs": set_sidebar("/front_end/nodejs"),
-      "/backend/JavaScript": set_sidebar("/backend/JavaScript"),
-      "/backend/html5": set_sidebar("/backend/html5"),
-      "/backend/vue2": set_sidebar("/backend/vue2"),
-      "/backend/react": set_sidebar("/backend/react"),
-      "/backend/Interview_about": set_sidebar("/backend/Interview_about"),
-      "/tool_docs/git": set_sidebar("/tool_docs/git"),
+      // 默认关闭侧边栏
+      if (
+        !Object.keys(sidebarConfig).some((prefix) => path.startsWith(prefix))
+      ) {
+        return false;
+      }
+
+      // 为匹配的路径启用侧边栏
+      return sidebarConfig[path];
     },
-    sidebar: false, // 关闭侧边栏
     aside: "left", // 设置右侧侧边栏在左侧显示
     socialLinks: [{ icon: "github", link: "https://github.com/nbkelasi" }],
     footer: {
